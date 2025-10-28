78205a5a1b5d8cbda7dae3a54ea41ed1bc486e4f
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-28 19:16:02 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,42 @@ public class Main {
             int remainingDistance = marafonDistance1 - distance1;
         System.out.println("Держитесь! Осталось " + remainingDistance + " метров");
         }
+//task 3
+        int budget = 1000;
+        int dailyCost = 100;
+        int currentDay = 0;
+        int remainingBudget = budget;
+        System.out.println("Бюджет: " + budget + " ₽");
+        System.out.println("Расчет дней парковки...\n");
+        while (remainingBudget >= dailyCost) {
+            currentDay++;
+            if (currentDay % 5 == 0) {
+                System.out.println("День " + currentDay + ":  БЕСПЛАТНО (5-й день)");
+                continue;
+            }
+            remainingBudget -= dailyCost;
+            System.out.println("День " + currentDay + ": Оплачено 100 ₽ (осталось: " + remainingBudget + " ₽)");
+        }
+        System.out.println("\n ИТОГ:");
+        System.out.println("На " + budget + " ₽ можно оставить автомобиль на " + currentDay + " дней");
+        System.out.println("Оставшийся бюджет: " + remainingBudget + " ₽");
+        int budget1 = 1000;
+        int dailyCost1 = 100;
+        int remainingBudget1 = budget;
+        int maxDays1 = 0;
+        for (int day = 1; remainingBudget1 >= dailyCost; day++) {
+            if (day % 5 == 0) {
+                System.out.println("День " + day + ":  БЕСПЛАТНО (5-й день)");
+                maxDays1 = day;
+                continue;
+            }
+            remainingBudget1 -= dailyCost;
+            maxDays1 = day;
+            System.out.println("День " + day + ":  Оплачено 100 ₽ (осталось: " + remainingBudget1 + " ₽)");
+        }
+        System.out.println("\n ИТОГ:");
+        System.out.println("На " + budget1 + " ₽ можно оставить автомобиль на " + maxDays1 + " дней");
+        System.out.println("Оставшийся бюджет: " + remainingBudget1 + " ₽");
     }
 }
 
